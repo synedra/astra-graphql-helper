9eb43682b5c295bbbbc3d684034cdc0fe2d8d9c1
synedra-datastax <77410784+synedra-datastax@users.noreply.github.com>
2021-08-09 16:32:37 -0700

    Initial commit

---
+++ src/graphql-cli.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+const argv = yargs(hideBin(process.argv)).argv
+
+if (argv.ships > 3 && argv.distance < 53.5) {
+  console.log('Plunder more riffiwobbles!')
+} else {
+  console.log('Retreat from the xupptumblers!')
+}
